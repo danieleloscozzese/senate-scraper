b4c5180880133d25bbbea54d5053c5289b69fb4b
Daniel Arthur Gallagher <8526031+DanArthurGallagher@users.noreply.github.com>
2021-03-23 20:37:05 +0000

    chore: build the base app

---
+++ src/index.js
@@ -0,0 +1,104 @@
+const { Command } = require("commander");
+const axios = require("axios").default;
+const jsdom = require("jsdom");
+const config = require("../regions.json");
+const { writeFile } = require("fs/promises");
+
+const { JSDOM } = jsdom;
+
+const program = new Command();
+
+program.version("1.0.0");
+
+program.parse(process.argv);
+
+axios
+  .get(config.lombardia)
+  .then((response) => {
+    const dom = new JSDOM(response.data);
+
+    const links = dom.window.document.querySelectorAll(
+      ".linkSenatore a[href*=sattsen]"
+    );
+
+    /** @type {String[]} */
+    const senatorPages = [];
+
+    for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
+      /** @type {string} */
+      const ref = links[linkIndex].href;
+
+      if (ref.startsWith("http")) {
+        senatorPages.push(ref);
+      } else {
+        senatorPages.push(`http://www.senato.it${ref}`);
+      }
+    }
+
+    console.log("Number of pages", senatorPages.length);
+
+    return senatorPages;
+  })
+  .then((senatorPages) => {
+    const mailRequests = senatorPages.map((url) => {
+      return axios
+        .get(url, {
+          maxRedirects: 0,
+        })
+        .catch((result) => {
+          /** @type {string} */
+          const rawLocation = result.response.headers["location"];
+
+          if (result.response.status === 307 && rawLocation) {
+            let loc = rawLocation;
+            if (rawLocation.startsWith("http:")) {
+              loc = rawLocation.replace("http", "https");
+            }
+
+            return axios.get(loc);
+          } else {
+            throw new Error(
+              `Expected a redirect but the status was ${result.status} and the headers are ${result.headers}`
+            );
+          }
+        })
+        .then((response) => {
+          const mailLine = response.data
+            .split("\n")
+            .filter((s) => s.includes("cnt_email"));
+
+          if (mailLine.length !== 1) {
+            throw new RangeError("There are too many matching lines, or 0.");
+          }
+
+          const address = /\s'(.*@.*)'\s/.exec(mailLine)[1];
+
+          return address;
+        })
+        .catch((e) => {
+          console.error(`Processing issue with ${url}: ${e}`);
+        })
+        .catch((e) => {
+          console.error(`Error retrieving ${url}: ${e}`);
+        });
+    });
+
+    Promise.all(mailRequests)
+      .then((mails) => {
+        return mails.filter(Boolean).join("\n");
+      })
+      .then((list) =>
+        writeFile(`${__dirname}/../list.txt`, list, "utf8")
+          .then(() => {
+            console.log("All written");
+          })
+          .catch((e) => {
+            console.error("Error writing");
+            console.error(e);
+          })
+      );
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exitCode = 1;
+  });
