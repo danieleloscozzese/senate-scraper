6c1949b4fb960fd6ca6c5e893109acaeb3df33e0
Daniel Arthur Gallagher <8526031+danieleloscozzese@users.noreply.github.com>
2024-03-29 09:35:06 +0000

    feat: rewrite the program

--- src/index.mjs
+++ src/index.mjs
@@ -1,114 +1,113 @@
-// @ts-check
 import { Command } from "commander";
 import { JSDOM } from "jsdom";
-import { readFile, writeFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
+/** @type {Record<string, string>} */
+import config from "../regions.json" assert { type: "json" };
 
-const config = JSON.parse(
-  await readFile(new URL("../regions.json", import.meta.url))
-);
-
-const program = new Command();
+const program = new Command("Senate Scraper");
 
 program.version("1.0.0");
 
 program.parse(process.argv);
 
 fetch(config.lombardia)
-  .then((res) => {
-    if (res.ok) {
-      return res.text();
-    } else {
-      throw new Error(res.statusText);
-    }
-  })
-  .then((data) => {
-    const dom = new JSDOM(data);
+	.then((res) => {
+		if (res.ok) {
+			return res.text();
+		} else {
+			throw new Error(res.statusText);
+		}
+	})
+	.then((data) => {
+		const dom = new JSDOM(data);
 
-    const links = dom.window.document.querySelectorAll(
-      ".linkSenatore a[href*=sattsen]"
-    );
+		/** @type NodeListOf<HTMLAnchorElement> */
+		const links = dom.window.document.querySelectorAll(
+			'.linkSenatore a[href*="senatori"]',
+		);
 
-    /** @type {String[]} */
-    const senatorPages = [];
+		/** @type {URL[]} */
+		const senatorPages = [];
 
-    for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
-      /** @type {string} */
-      const ref = links[linkIndex].href;
+		for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
+			/** @type {string} */
+			const ref = links[linkIndex].href;
 
-      if (ref.startsWith("http")) {
-        senatorPages.push(ref);
-      } else {
-        senatorPages.push(`http://www.senato.it${ref}`);
-      }
-    }
+			if (!ref.startsWith("/")) {
+				console.warn("Unexpected URL, not relative:", ref);
+			} else {
+				senatorPages.push(new URL(ref, "https://www.senato.it/"));
+			}
+		}
 
-    console.log("Number of pages", senatorPages.length);
+		console.log("Number of pages", senatorPages.length);
 
-    return senatorPages;
-  })
-  .then((senatorPages) => {
-    const mailRequests = senatorPages.map((url) => {
-      return fetch(url)
-        .then((result) => {
-          /** @type {string} */
-          const rawLocation = result.headers.get("Location");
+		return senatorPages;
+	})
+	.then((senatorPages) => {
+		const mailRequests = senatorPages.map((url) => {
+			return fetch(url)
+				.then((result) => {
+					if (result.ok && result.status === 200) {
+						return result.text();
+					} else if (result.status === 307) {
+						let loc = result.headers.get("Location");
+						if (loc === null) {
+							throw new RangeError("Unexpected result, cannot follow:", result);
+						} else if (loc.startsWith("http:")) {
+							loc = loc.replace("http", "https");
+						}
 
-          if (result.status === 307 && rawLocation) {
-            let loc = rawLocation;
-            if (rawLocation.startsWith("http:")) {
-              loc = rawLocation.replace("http", "https");
-            }
+						return fetch(loc).then((res) => {
+							if (res.ok) {
+								return res.text();
+							} else {
+								throw new Error(res.statusText);
+							}
+						});
+					} else {
+						throw new Error(
+							`Expected a redirect but the status was ${result.status} and the headers are ${result.headers}`,
+						);
+					}
+				})
+				.then((page) => {
+					const mailLine = page
+						.split("\n")
+						.filter((s) => s.includes("cnt_email"));
 
-            return fetch(loc).then((res) => {
-              if (res.ok) {
-                return res.text();
-              } else {
-                throw new Error(res.statusText);
-              }
-            });
-          } else {
-            throw new Error(
-              `Expected a redirect but the status was ${result.status} and the headers are ${result.headers}`
-            );
-          }
-        })
-        .then((data) => {
-          const mailLine = data
-            .split("\n")
-            .filter((s) => s.includes("cnt_email"));
+					if (mailLine.length !== 1) {
+						throw new RangeError("There are too many matching lines, or 0.");
+					}
 
-          if (mailLine.length !== 1) {
-            throw new RangeError("There are too many matching lines, or 0.");
-          }
+					const address = /mailto:(.*@senato.it)"/.exec(mailLine)[1];
 
-          const address = /\s'(.*@.*)'\s/.exec(mailLine)[1];
+					return address;
+				})
+				.catch((e) => {
+					console.error(`Processing issue with ${url}: ${e}`);
+				})
+				.catch((e) => {
+					console.error(`Error retrieving ${url}: ${e}`);
+				});
+		});
 
-          return address;
-        })
-        .catch((e) => {
-          console.error(`Processing issue with ${url}: ${e}`);
-        })
-        .catch((e) => {
-          console.error(`Error retrieving ${url}: ${e}`);
-        });
-    });
-
-    Promise.all(mailRequests)
-      .then((mails) => {
-        return mails.filter(Boolean).join("\n");
-      })
-      .then((list) =>
-        writeFile(new URL("../list.txt", import.meta.url), list, "utf8")
-          .then(() => {
-            console.log("All written");
-          })
-          .catch((e) => {
-            console.error("Error writing");
-            console.error(e);
-          })
-      );
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exitCode = 1;
-  });
+		Promise.all(mailRequests)
+			.then((mails) => {
+				return mails.filter(Boolean).join("\n");
+			})
+			.then((list) =>
+				writeFile(new URL("../list.txt", import.meta.url), list, "utf8")
+					.then(() => {
+						console.log("All written");
+					})
+					.catch((e) => {
+						console.error("Error writing");
+						console.error(e);
+					}),
+			);
+	})
+	.catch((e) => {
+		console.error(e);
+		process.exitCode = 1;
+	});
